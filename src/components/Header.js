9fbe3163a7fede466a7089c00171ba3943258dcd
Thiago Capistrano <thiago@capistrano.net.br>
2019-06-03 08:58:31 -0300

    dev

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 
 export class Header extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  open = e => {
-    e.preventDefault();
-    this.props.history.push('/');
-  };
-
   render() {
     return (
       <header>
-        <h1 onClick={e => this.open(e)}>
-          <b>BUSCA KITSU</b> TESTE FRONT-END
+        <h1>
+          <a href="/">
+            <b>BUSCA KITSU</b> TESTE FRONT-END
+          </a>
         </h1>
         <h2>THIAGO CAPISTRANO</h2>
       </header>
